a3a2ecddef2291d59ce0216cda9ba4794fdf4b1c
taiyo2009 <huytaiyo@gmail.com>
2025-03-29 15:38:47 +0700

    add page home

--- HP9/settings.py
+++ HP9/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bai2'
+    'bai2',
+    'introduction'
 ]
 
 MIDDLEWARE = [
--- HP9/urls.py
+++ HP9/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("introduction/", include('introduction.urls')),
+    path('',include('introduction.urls')),
 ]
